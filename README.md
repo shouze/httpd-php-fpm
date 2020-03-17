97fb8af0a5b6a69d2c4dab0f11391274150f79da
Sébastien HOUZÉ <sebastien.houze@french-hospitality.fr>
2020-03-17 17:17:01 +0100

    feat: static http/1.1 server

---
+++ README.md
@@ -0,0 +1,64 @@
+# httpd-php-fpm
+
+Lightweight 🍃 fast ⚡ fastcgi php 🐘 proxy and static http server.
+
+## Why another http server for php?
+
+Because nowaday either you go serverless, either you manage distributed services 
+in containers. httpd-php-fpm focus on containerized php world.
+
+Well known existing solutions are:
+  - [apache + mod-php](https://hub.docker.com/layers/php/library/php/7.4-apache/images/sha256-48dde1707d7dca2b701aa230344c58cb8ec5b0ce8e9dbceced65bec5ccd7d1d0?context=explore): the more official & recommended solution. Unfortunately, 
+    this automatically imply a debian based docker image, pretty heavy (>=140Mib) 😭
+  - [nginx + php-fpm](https://hub.docker.com/layers/webdevops/php-nginx/alpine/images/sha256-6cada7ab54b149645ea149dde876c70b60a44869c14a360fda328fabb357e2ed?context=explore): the bad is that to avoid to violate the container's single
+    root process principle... we also need an init to supervise both nginx & 
+    php processes, and the resulting base docker image is still heavy, even an 
+    alpine variant (>=90Mib) 😭
+  - [symfony cli](https://github.com/symfony/cli/releases): not recommended 
+    for production, not single responsibility (also embed Symfony Cloud utils), 
+    closed source and written in Go (hello garbage collector). It also try to 
+    bring the better developer experience so it does some voodoo, trying to make 
+    things work whatever your php setup is.
+
+So, here's the budget of this project:
+- Lightweight 🍃: base docker image less than 40Mib (compressed). And lowest memory footprint.
+- fast ⚡:
+  - at least 2k req/s under 2ms at 95 percentile for static files
+  - at least 1k req/s under 5ms at 95 percentile for basic php-fpm request
+
+## Inspirations
+
+- [kamasu](https://github.com/hhatto/kamasu)
+- [rust-httpd](https://github.com/PritiKumr/rust-httpd/blob/master/src/main.rs)
+- [fastcgi-serve](https://github.com/beberlei/fastcgi-serve)
+
+## Compiling
+
+```fish
+cargo build -j(nproc) --release
+# then run it, for example if you have a www dir:
+# ./target/release/httpd-php-fpm -d ./www
+```
+
+## Benchmarks
+
+### Static files
+
+current benchmarks, done with [vegeta](https://github.com/tsenart/vegeta) on a 
+8 core intel 10th gen CPU.
+
+```shell
+➜ echo "GET http://localhost:3000/" | vegeta attack -duration=10s -rate=2000 | tee results.bin | vegeta report
+Requests      [total, rate, throughput]         20000, 2000.13, 2000.08
+Duration      [total, attack, wait]             10s, 9.999s, 287.523µs
+Latencies     [min, mean, 50, 90, 95, 99, max]  219.856µs, 564.652µs, 312.437µs, 813.232µs, 1.763ms, 5.4ms, 21.491ms
+Bytes In      [total, mean]                     256240000, 12812.00
+Bytes Out     [total, mean]                     0, 0.00
+Success       [ratio]                           100.00%
+Status Codes  [code:count]                      200:20000  
+Error Set:
+```
+
+### php-fpm
+
+stay tuned!
---
+++ src/main.rs
@@ -0,0 +1,171 @@
+#![deny(warnings)]
+
+use std::convert::Infallible;
+use std::ffi::OsStr;
+use std::path::Path;
+use std::path::PathBuf;
+use std::sync::Arc;
+use tokio::fs::File;
+use tokio::io::AsyncReadExt;
+
+use clap::{App, Arg};
+
+use hyper::service::{make_service_fn, service_fn};
+use hyper::{Body, Request, Response, Server, StatusCode};
+
+const APP_NAME: &'static str = env!("CARGO_PKG_NAME");
+const VERSION: &'static str = env!("CARGO_PKG_VERSION");
+static NOTFOUND: &[u8] = b"Not Found";
+static INTERNAL_SERVER_ERROR: &[u8] = b"Internal Server Error";
+
+struct Config {
+    docroot: PathBuf,
+}
+
+async fn handle_request(config: &Config, req: Request<Body>) -> Result<Response<Body>, Infallible> {
+    let filename = config.docroot.join(&req.uri().path()[1..]);
+    println!(
+        "{} {}{}",
+        req.method(),
+        req.uri().path(),
+        req.uri().query().unwrap_or("")
+    );
+    match filename.exists() {
+        true => match filename.extension() != Some(OsStr::new("php")) {
+            true => Ok(serve_static_file(&filename).await),
+            _ => Ok(fastcgi_proxy().await),
+        },
+        _ => Ok(fastcgi_proxy().await),
+    }
+}
+
+async fn fastcgi_proxy() -> Response<Body> {
+    // TODO: use hyper client here to proxy to php-fpm spawned process
+    return not_found();
+}
+
+async fn serve_static_file(filename: &Path) -> Response<Body> {
+    //   TODO:
+    //   - mime types when serving static files
+    //   - remove some debugs println
+    println!("serving {}", filename.display());
+    if let Ok(mut file) = File::open(filename).await {
+        let mut buf = Vec::new();
+        if let Ok(_) = file.read_to_end(&mut buf).await {
+            return Response::new(buf.into());
+        }
+
+        let index_html = filename.join("index.html");
+        let mut resolved_filename = match index_html.is_file() {
+            true => index_html,
+            _ => filename.to_path_buf(),
+        };
+
+        if resolved_filename == filename {
+            let index_htm = filename.join("index.htm");
+            resolved_filename = match index_htm.is_file() {
+                true => index_htm,
+                _ => filename.to_path_buf(),
+            };
+        }
+
+        if resolved_filename == filename {
+            return internal_server_error();
+        }
+
+        println!("serving {}", resolved_filename.display());
+        if let Ok(mut file) = File::open(resolved_filename).await {
+            if let Ok(_) = file.read_to_end(&mut buf).await {
+                return Response::new(buf.into());
+            }
+
+            return internal_server_error();
+        }
+
+        return internal_server_error();
+    }
+    return not_found();
+}
+
+/// HTTP status code 404
+fn not_found() -> Response<Body> {
+    Response::builder()
+        .status(StatusCode::NOT_FOUND)
+        .body(NOTFOUND.into())
+        .unwrap()
+}
+
+/// HTTP status code 500
+fn internal_server_error() -> Response<Body> {
+    Response::builder()
+        .status(StatusCode::INTERNAL_SERVER_ERROR)
+        .body(INTERNAL_SERVER_ERROR.into())
+        .unwrap()
+}
+
+async fn shutdown_signal() {
+    tokio::signal::ctrl_c()
+        .await
+        .expect("failed to install CTRL+C signal handler");
+}
+
+#[tokio::main]
+pub async fn main() {
+    let app = App::new(APP_NAME)
+        .version(VERSION)
+        .about("Lightweight 🍃 fast ⚡ fastcgi php 🐘 proxy and static http server.")
+        .arg(
+            Arg::with_name("serve")
+                .short("s")
+                .long("serve")
+                .takes_value(true)
+                .default_value("127.0.0.1:3000")
+                .value_name("ADDR")
+                .help("Serve on address and port"),
+        )
+        .arg(
+            Arg::with_name("docroot")
+                .short("d")
+                .long("document-root")
+                .takes_value(true)
+                .value_name("DIR")
+                .default_value("./")
+                .help("Document root of both static files and\nphp-fpm"),
+        );
+
+    let matches = app.get_matches();
+
+    let addr = match matches.value_of("serve") {
+        Some(v) => v.to_string().parse().unwrap(),
+        None => "127.0.0.1:3000".to_string().parse().unwrap(),
+    };
+
+    let docroot = match matches.value_of("docroot") {
+        Some(v) => v,
+        None => "./",
+    };
+    let d = Path::new(docroot).to_owned();
+    if !d.is_dir() {
+        panic!("docroot {} is a non existing directory.", docroot);
+    }
+    let config = Arc::new(Config { docroot: d });
+    let make_svc = make_service_fn(|_| {
+        let onion1 = Arc::clone(&config);
+        async move {
+            Ok::<_, Infallible>(service_fn(move |req: Request<Body>| {
+                let onion2 = Arc::clone(&onion1);
+                async move { handle_request(&*onion2, req).await }
+            }))
+        }
+    });
+
+    // TODO: spawn php-fpm master process
+    let server = Server::bind(&addr).tcp_nodelay(true).serve(make_svc);
+
+    println!("Listening on http://{},\nserving docroot {}", addr, docroot);
+    let graceful = server.with_graceful_shutdown(shutdown_signal());
+
+    if let Err(e) = graceful.await {
+        eprintln!("server error: {}", e);
+    }
+}
